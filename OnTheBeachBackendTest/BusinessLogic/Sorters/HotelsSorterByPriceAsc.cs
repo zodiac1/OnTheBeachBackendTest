d9030f4e0994fe24365588a6343bc979bd9005ea
Belal Uddin <belal1@hotmail.co.uk>
2024-03-17 14:26:38 +0000

    Adds hotel sort by price feature and test

---
+++ OnTheBeachBackendTest/BusinessLogic/Sorters/HotelsSorterByPriceAsc.cs
@@ -0,0 +1,13 @@
+﻿using OnTheBeachBackendTest.Entities;
+using OnTheBeachBackendTest.Types.Sorters;
+
+namespace OnTheBeachBackendTest.BusinessLogic.Sorters
+{
+    public class HotelsSorterByPriceAsc : ISorter<Hotel>
+    {
+        public IEnumerable<Hotel> Sort(IEnumerable<Hotel> hotels)
+        {
+            return hotels.OrderBy(hotel => hotel.PricePerNight);
+        }
+    }
+}
---
+++ OnTheBeachBackendTest/UnitTests/Sorters/HotelsSorterByPriceAscTests.cs
@@ -0,0 +1,57 @@
+﻿using OnTheBeachBackendTest.BusinessLogic.DataSources;
+using OnTheBeachBackendTest.BusinessLogic.Sorters;
+using OnTheBeachBackendTest.Entities;
+using System.Text.Json;
+
+namespace OnTheBeachBackendTest.UnitTests.Sorters
+{
+    public class HotelsSorterByPriceAscTests
+    {
+        private IList<Hotel>? TestHotels;
+
+        [SetUp]
+        public void Setup()
+        {
+            using (var reader = new StreamReader("hotel-data.json"))
+            {
+                var json = reader.ReadToEnd();
+                var hotelsRaw = JsonSerializer.Deserialize<List<Dictionary<string, dynamic>>>(json);
+
+                TestHotels = new List<Hotel>();
+
+                foreach (var item in hotelsRaw)
+                {
+                    JsonElement localAirportsRaw = item["local_airports"];
+                    string[] localAirports = new string[localAirportsRaw.GetArrayLength()];
+                    var enumerator = localAirportsRaw.EnumerateArray();
+
+                    var i = 0;
+
+                    while (enumerator.MoveNext())
+                    {
+                        localAirports[i++] = enumerator.Current.GetString()!;
+                    }
+
+
+                    TestHotels.Add(new Hotel { Id = item["id"].GetInt32(), Name = item["name"].GetString(), PricePerNight = item["price_per_night"].GetDouble(), ArrivalDate = DateTime.Parse(item["arrival_date"].GetString()), LocalAirports = localAirports, Nights = item["nights"].GetInt32() });
+                }
+            }
+        }
+
+        [Test]
+        public void Sort_HotelsSorterByPriceAsc_ReturnsAllHotelsByPriceAsc()
+        {
+            //Arrange
+            var hotelsSorterByPriceAsc = new HotelsSorterByPriceAsc();
+
+            //Act
+            var sortedHotels = hotelsSorterByPriceAsc.Sort(TestHotels);
+
+            //Assert
+            Assert.True(sortedHotels.Any());
+            Assert.True(sortedHotels.Count() == 13);
+            Assert.True(sortedHotels.First().Id == 8);
+            Assert.True(sortedHotels.First().PricePerNight == 45);
+        }
+    }
+}
