7df904c0829f9cc0ca3b6934b77651db6f2e5d8a
Belal Uddin <belal1@hotmail.co.uk>
2024-03-17 14:14:16 +0000

    Adds flight sort by price feature and test

---
+++ OnTheBeachBackendTest/BusinessLogic/Sorters/FlightsSorterByPriceAsc.cs
@@ -0,0 +1,13 @@
+﻿using OnTheBeachBackendTest.Entities;
+using OnTheBeachBackendTest.Types.Sorters;
+
+namespace OnTheBeachBackendTest.BusinessLogic.Sorters
+{
+    public class FlightsSorterByPriceAsc : ISorter<Flight>
+    {
+        public IEnumerable<Flight> Sort(IEnumerable<Flight> flights)
+        {
+            return flights.OrderBy(flight => flight.Price);
+        }
+    }
+}
---
+++ OnTheBeachBackendTest/UnitTests/Sorters/FlightsSorterByPriceAscTests.cs
@@ -0,0 +1,47 @@
+﻿using OnTheBeachBackendTest.BusinessLogic.DataSources;
+using OnTheBeachBackendTest.BusinessLogic.Sorters;
+using OnTheBeachBackendTest.Entities;
+using System.Text.Json;
+
+namespace OnTheBeachBackendTest.UnitTests.Sorters
+{
+    public class FlightsSorterByPriceAscTests
+    {
+        private IList<Flight>? TestFlights;
+
+        [SetUp]
+        public void Setup()
+        {
+            using (var reader = new StreamReader("flight-data.json"))
+            {
+                var json = reader.ReadToEnd();
+                var flightsRaw = JsonSerializer.Deserialize<List<Dictionary<string, dynamic>>>(json);
+
+                TestFlights = new List<Flight>();
+
+                foreach (var item in flightsRaw)
+                {
+                    TestFlights.Add(new Flight { Id = item["id"].GetInt32(), Airline = item["airline"].GetString(), From = item["from"].GetString(), To = item["to"].GetString(), Price = item["price"].GetDouble(), DepartureDate = DateTime.Parse(item["departure_date"].GetString()) });
+                }
+            }
+        }
+
+        [Test]
+        public void Sort_FlightsByPriceAsc_ReturnsAllFlightsByPriceAsc()
+        {
+            //Arrange
+            var allFlightsData = new AllFlightsData { Flights = TestFlights };
+            var allFlights = allFlightsData.GetData();
+            var flightsSorterByPriceAsc = new FlightsSorterByPriceAsc();
+
+            //Act
+            var sortedFlights = flightsSorterByPriceAsc.Sort(allFlights);
+
+            //Assert
+            Assert.True(sortedFlights.Any());
+            Assert.True(sortedFlights.Count() == 12);
+            Assert.True(sortedFlights.First().Id == 6);
+            Assert.True(sortedFlights.First().Price == 75);
+        }
+    }
+}
