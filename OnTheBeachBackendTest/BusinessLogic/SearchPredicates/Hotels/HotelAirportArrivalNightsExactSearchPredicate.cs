31f2fe70a0f342f3f79c42a4dc82a718aae52183
Belal Uddin <belal1@hotmail.co.uk>
2024-03-17 16:07:01 +0000

    Adds hotel search predicate with unit tests

---
+++ OnTheBeachBackendTest/BusinessLogic/SearchPredicates/Hotels/HotelAirportArrivalNightsExactSearchPredicate.cs
@@ -0,0 +1,24 @@
+﻿using OnTheBeachBackendTest.Entities;
+using OnTheBeachBackendTest.Types.SearchPredicates;
+
+namespace OnTheBeachBackendTest.BusinessLogic.SearchPredicates.Hotels
+{
+    public class HotelAirportArrivalNightsExactSearchPredicate : ISearchPredicate<Hotel>
+    {
+        public required string Airport { get; set; }
+        public required int Nights { get; set; }
+        public required DateTime ArrivalDate { get; set; }
+
+        public bool IsMatch(Hotel hotel)
+        {
+            return
+                hotel != null &&
+                DateTime.Compare(hotel.ArrivalDate, ArrivalDate) == 0 &&
+                hotel.Nights == Nights &&
+                hotel.LocalAirports != null &&
+                hotel.LocalAirports.Any(localAirport => localAirport.Equals(Airport, StringComparison.OrdinalIgnoreCase))
+            ;
+
+        }
+    }
+}
---
+++ OnTheBeachBackendTest/UnitTests/SearchPredicates/Hotels/HotelAirportArrivalNightsExactSearchPredicateTests.cs
@@ -0,0 +1,46 @@
+﻿using OnTheBeachBackendTest.BusinessLogic.SearchPredicates.Hotels;
+using OnTheBeachBackendTest.Data;
+using OnTheBeachBackendTest.Entities;
+
+namespace OnTheBeachBackendTest.UnitTests.SearchPredicates.Hotels
+{
+    public class HotelAirportArrivalNightsExactSearchPredicateTests
+    {
+        private IList<Hotel>? TestHotelsData;
+
+        [SetUp]
+        public void Setup()
+        {
+            TestHotelsData = Helpers.GetTestHotelsData();
+        }
+
+        [Test]
+        public void IsMatch_TestHotel_ReturnsTrue()
+        {
+            //Arrange
+            var testHotel = TestHotelsData[1];
+            var hotelPredicate = new HotelAirportArrivalNightsExactSearchPredicate { Airport = testHotel.LocalAirports[0], ArrivalDate = testHotel.ArrivalDate, Nights = testHotel.Nights };
+
+            //Act
+            var result = hotelPredicate.IsMatch(testHotel);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsMatch_FakeHotel_ReturnsFalse()
+        {
+            //Arrange
+            var testHotel = TestHotelsData[1];
+            var fakeHotel = new Hotel { Id = 0, ArrivalDate = DateTime.Now, LocalAirports = [], Name = "", Nights = 1, PricePerNight = 100 };
+            var hotelPredicate = new HotelAirportArrivalNightsExactSearchPredicate { Airport = testHotel.LocalAirports[0], ArrivalDate = testHotel.ArrivalDate, Nights = testHotel.Nights };
+
+            //Act
+            var result = hotelPredicate.IsMatch(fakeHotel);
+
+            //Assert
+            Assert.False(result);
+        }
+    }
+}
