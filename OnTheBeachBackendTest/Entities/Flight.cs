f114702bf218957bc758d8a14d18488dfeea6665
Belal Uddin <belal1@hotmail.co.uk>
2024-03-17 13:10:01 +0000

    Adds entities

---
+++ OnTheBeachBackendTest/Entities/Flight.cs
@@ -0,0 +1,12 @@
+﻿namespace OnTheBeachBackendTest.Entities
+{
+    public class Flight
+    {
+        public required int Id { get; set; }
+        public required string Airline { get; set; }
+        public required string From { get; set; }
+        public required string To { get; set; }
+        public required double Price { get; set; }
+        public required DateTime DepartureDate { get; set; }
+    }
+}
---
+++ OnTheBeachBackendTest/Entities/Hotel.cs
@@ -0,0 +1,12 @@
+﻿namespace OnTheBeachBackendTest.Entities
+{
+    public class Hotel
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required DateTime ArrivalDate { get; set; }
+        public required double PricePerNight { get; set; }
+        public required string[] LocalAirports { get; set; }
+        public required int Nights { get; set; }
+    }
+}
