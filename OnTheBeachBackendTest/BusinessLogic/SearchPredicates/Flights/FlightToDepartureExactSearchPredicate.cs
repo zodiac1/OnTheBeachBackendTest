c6bcd11485e3c57fbbb23ff7f9334cf20b06d3a0
Belal Uddin <belal1@hotmail.co.uk>
2024-03-17 15:57:33 +0000

    Adds flight search predicate with unit tests

---
+++ OnTheBeachBackendTest/BusinessLogic/SearchPredicates/Flights/FlightToDepartureExactSearchPredicate.cs
@@ -0,0 +1,21 @@
+﻿using OnTheBeachBackendTest.Entities;
+using OnTheBeachBackendTest.Types.SearchPredicates;
+
+namespace OnTheBeachBackendTest.BusinessLogic.SearchPredicates.Flights
+{
+    public class FlightToDepartureExactSearchPredicate : ISearchPredicate<Flight>
+    {
+        public required string To { get; set; }
+        public required DateTime DepartureDate { get; set; }
+
+        public bool IsMatch(Flight flight)
+        {
+            return
+                flight != null &&
+                string.Equals(flight.To, To, StringComparison.OrdinalIgnoreCase) &&
+                DateTime.Compare(flight.DepartureDate, DepartureDate) == 0
+            ;
+
+        }
+    }
+}
---
+++ OnTheBeachBackendTest/UnitTests/SearchPredicates/Flights/FlightToDepartureExactSearchPredicateTests.cs
@@ -0,0 +1,46 @@
+﻿using OnTheBeachBackendTest.BusinessLogic.SearchPredicates.Flights;
+using OnTheBeachBackendTest.Data;
+using OnTheBeachBackendTest.Entities;
+
+namespace OnTheBeachBackendTest.UnitTests.SearchPredicates.Flights
+{
+    public class FlightToDepartureExactSearchPredicateTests
+    {
+        private IList<Flight>? TestFlightsData;
+
+        [SetUp]
+        public void Setup()
+        {
+            TestFlightsData = Helpers.GetTestFlightsData();
+        }
+
+        [Test]
+        public void IsMatch_TestFlight_ReturnsTrue()
+        {
+            //Arrange
+            var testFlight = TestFlightsData[1];
+            var flightPredicate = new FlightToDepartureExactSearchPredicate { DepartureDate = testFlight.DepartureDate, To = testFlight.To };
+
+            //Act
+            var result = flightPredicate.IsMatch(testFlight);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsMatch_FakeFlight_ReturnsFalse()
+        {
+            //Arrange
+            var testFlight = TestFlightsData[1];
+            var fakeFlight = new Flight { Airline = "", DepartureDate = DateTime.Now, From = "", Id = 0, Price = 0, To = "" };
+            var flightPredicate = new FlightToDepartureExactSearchPredicate { DepartureDate = testFlight.DepartureDate, To = testFlight.To };
+
+            //Act
+            var result = flightPredicate.IsMatch(fakeFlight);
+
+            //Assert
+            Assert.False(result);
+        }
+    }
+}
