6707272f1884a229c96643584a3a9a9768322aad
Belal Uddin <belal1@hotmail.co.uk>
2024-03-17 16:01:00 +0000

    Adds flight search predicate with unit tests

---
+++ OnTheBeachBackendTest/BusinessLogic/SearchPredicates/Flights/FlightToFromDepartureExactSearchPredicate.cs
@@ -0,0 +1,23 @@
+﻿using OnTheBeachBackendTest.Entities;
+using OnTheBeachBackendTest.Types.SearchPredicates;
+
+namespace OnTheBeachBackendTest.BusinessLogic.SearchPredicates.Flights
+{
+    public class FlightToFromDepartureExactSearchPredicate : ISearchPredicate<Flight>
+    {
+        public required string To { get; set; }
+        public required string From { get; set; }
+        public required DateTime DepartureDate { get; set; }
+
+        public bool IsMatch(Flight flight)
+        {
+            return
+                flight != null &&
+                string.Equals(flight.To, To, StringComparison.OrdinalIgnoreCase) &&
+                string.Equals(flight.From, From, StringComparison.OrdinalIgnoreCase) &&
+                DateTime.Compare(flight.DepartureDate, DepartureDate) == 0
+            ;
+
+        }
+    }
+}
